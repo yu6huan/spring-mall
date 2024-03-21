c10957161c346e20d8d65b8b42293d65bbcd3812
Ray <ray123456789123456@gmail.com>
2024-03-21 21:52:08 +0800

    帳號功能 - 登入

--- src/main/java/com/liuray/springbootmall/controller/UserController.java
+++ src/main/java/com/liuray/springbootmall/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.liuray.springbootmall.controller;
 
+import com.liuray.springbootmall.dto.UserLoginRequest;
 import com.liuray.springbootmall.dto.UserRegisterRequest;
 import com.liuray.springbootmall.model.User;
 import com.liuray.springbootmall.service.UserService;
@@ -25,5 +26,12 @@ public class UserController {
         User user = userService.getUserById(userId);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/users/login")
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
+        User user = userService.login(userLoginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
--- src/main/java/com/liuray/springbootmall/service/UserService.java
+++ src/main/java/com/liuray/springbootmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.liuray.springbootmall.service;
 
+import com.liuray.springbootmall.dto.UserLoginRequest;
 import com.liuray.springbootmall.dto.UserRegisterRequest;
 import com.liuray.springbootmall.model.User;
 
@@ -8,4 +9,6 @@ public interface UserService {
     User getUserById(Integer userId);
 
     Integer register(UserRegisterRequest userRegisterRequest);
+
+    User login(UserLoginRequest userLoginRequest);
 }
